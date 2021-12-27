71df669b06fce9bfac54988048f660f54bd8da9c
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-12-27 05:45:44 +0100

    nothing

--- JS/timeline.js
+++ JS/timeline.js
@@ -1,6 +1,7 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -14,16 +15,20 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("timelineContent");
-  
   var dots = document.getElementsByClassName("demo");
-  //var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
+  //var captionText = document.getElementById("caption"
+
+  if (n > slides.length) {
+    slideIndex =1
+  }
 
   if (n < 1) {slideIndex = slides.length}
+
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  
   
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
