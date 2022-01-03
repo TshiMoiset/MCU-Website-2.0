a072cd6a9decaa56b2db3fb3d111053a420e7410
Moise Tshiibwabwa <moisetshi@hotmail.com>
2022-01-03 18:32:22 +0100

    Work in progress

--- JS/timeline.js
+++ JS/timeline.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 
@@ -37,6 +37,6 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
    //captionText.innerHTML = dots[slideIndex-1].alt;
-}
+}*/
 
 
